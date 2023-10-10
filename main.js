5f4b225a9545201a746eb67902790bdce2885098
Bazz-tube <93840922+Bazz-tube@users.noreply.github.com>
2023-10-10 20:36:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+let number = 0
+
 function setimg(){
 
-    const number = document.getElementById("number").value
+    number = document.getElementById("number").value
+    const file = "./matematika/526021105-hrava-matematika_231010_105343-" + number.padStart(3, '0') + ".png"
+    document.getElementById("image").src = file
+
+}
+
+function changimg(by){
+
+    const numero = Number(number)+by
+    console.log(numero)
+    number = String(numero)
     const file = "./matematika/526021105-hrava-matematika_231010_105343-" + number.padStart(3, '0') + ".png"
     document.getElementById("image").src = file
 
